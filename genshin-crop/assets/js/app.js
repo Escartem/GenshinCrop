4250f1867633b6f52b0b71364be2b37fb54c83fe
Escartem <escartem.yt@gmail.com>
2022-11-18 18:27:21 +0100

    c2

--- genshin-crop/assets/js/app.js
+++ genshin-crop/assets/js/app.js
@@ -5,12 +5,14 @@ setupEMS(config)
 
 var curr_char = null;
 var buid = null;
+var url_object = null;
+var gen = false;
 
 regenBtn = document.getElementById("regenBtn");
 regenBtn.addEventListener("click", function(event) {event.preventDefault(); genImage();})
 
 reportBtn = document.getElementById("reportBtn");
-reportBtn.addEventListener("click", function(event) {event.preventDefault(); alert("Please join the discord server and provide this : "+buid)})
+reportBtn.addEventListener("click", function(event) {event.preventDefault(); showPopup("Report image", "Please join the <a href='https://discord.gg/fzRdtVh', target='_blank'>discord</a> and provide this : <span style='color: white; background: #2a2a2a;'>"+buid+"</span>", 260, 200);})
 
 checkInputBtn = document.getElementById("checkInputBtn");
 checkInputBtn.addEventListener("click", function(event) {event.preventDefault(); check();})
@@ -22,6 +24,7 @@ document.getElementById("uinput").onkeydown = function(event) {
 }
 
 function genImage() {
+    gen = true;
     document.getElementById("error-text").style.visibility = "hidden";
     document.getElementById("load-spinner").style.visibility = "visible";
     document.getElementById("char").style.opacity = 0.5;
@@ -59,10 +62,15 @@ function genImage() {
         reportBtn.disabled = false;
         reportBtn.style.filter = "brightness(1)";
 
-        document.getElementById("input-area").style.visibility = "visible";
-        document.getElementById("result-area").style.visibility = "hidden";
+        document.getElementById("input-area").style.opacity = 1;
+        document.getElementById("result-area").style.opacity = 0;
 
         document.getElementById("uinput").focus();
+
+        // for some reason this does not seem to work
+        URL.revokeObjectURL(url_object);
+
+        gen = false;
     }
 
     const req = new Request("https://hm3g1egfy6.execute-api.eu-west-3.amazonaws.com/v1/p/gca");
@@ -70,8 +78,9 @@ function genImage() {
     fetch(req).then((response) => {
         curr_char = response.headers.get("char");
         buid = response.headers.get("uid");
-        response.blob().then((myBlob) => {
-            const objectURL = URL.createObjectURL(myBlob);
+        response.blob().then((blob) => {
+            const objectURL = URL.createObjectURL(blob);
+            url_object = objectURL
             img.src = objectURL;
         });
     })
@@ -99,8 +108,47 @@ function check() {
 
     document.getElementById("uinput").value = "";
 
-    document.getElementById("input-area").style.visibility = "hidden";
-    document.getElementById("result-area").style.visibility = "visible";
+    document.getElementById("input-area").style.opacity = 0;
+    document.getElementById("result-area").style.opacity = 1;
 }
 
+document.addEventListener("keydown", function(e) {
+    if (e.ctrlKey && e.keyCode == 81) {
+        e.preventDefault();
+        if (gen == false) {
+            genImage();
+        }
+    }
+})
+
+function showPopup(title, text, width="150", height="90") {
+    var popupDialog = document.getElementById("popupDialog");
+
+    if (! popupDialog.showModal) {
+        dialogPolyfill.registerDialog(popupDialog);
+    }
+
+    document.getElementById("popupTitle").innerHTML = title;
+    document.getElementById("popupContent").innerHTML = text;
+
+    popupDialog.style.height = height + "px";
+    popupDialog.style.width = width + "px";
+
+    popupDialog.showModal();
+
+    popupDialog.querySelector('.close').addEventListener('click', function() {
+        popupDialog.close();
+    });
+}
+
+chars = "Aether <br/> Albedo <br/> Aloy <br/> Amber <br/> Itto <br/> Ayaka <br/> Ayato <br/> Barbara <br/> Beidou <br/> Bennett <br/> Chongyun <br/> Collei <br/> Diluc <br/> Diona <br/> Dori <br/> Eula <br/> Fischl <br/> Ganyu <br/> Gorou <br/> Heizou <br/> Hu Tao <br/> Jean <br/> Kaeya <br/> Kazuha <br/> Keqing <br/> Klee <br/> Kokomi <br/> Kuki Shinobu <br/> Lisa <br/> Mona <br/> Ningguang <br/> Noelle <br/> Qiqi <br/> Raiden <br/> Razor <br/> Rosaria <br/> Kujou Sara <br/> Sayu <br/> Shenhe <br/> Sucrose <br/> Tartaglia <br/> Thoma <br/> Tighnari <br/> Lumine <br/> Venti <br/> Xiangling <br/> Xiao <br/> Xingqiu <br/> Xinyan <br/> Yae Miko <br/> Yanfei <br/> Yelan <br/> Yoimiya <br/> Yun Jin <br/> Zhongli <br/><br/><br/><br/>"
+showCharsBtn = document.getElementById("showCharsBtn");
+showCharsBtn.addEventListener("click", function(event) {event.preventDefault(); showPopup("List of characters names", chars, 250, 500);})
+
 window.onload = genImage();
+
+// TODO
+// * blob delete ?
+// * confetis
+// * clean assets
+// * phone support
