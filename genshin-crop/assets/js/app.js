1a70e6732e6b1f274bc24f099e545e5a87695ece
Escartem <escartem.yt@gmail.com>
2022-11-19 00:41:19 +0100

    genshin crop initial release

--- genshin-crop/assets/js/app.js
+++ genshin-crop/assets/js/app.js
@@ -1,71 +1,83 @@
 // Escartem
 
-config = {"RCB": true, "DVT": false, "BSC": true, "SRC": true}
-setupEMS(config)
+setupEMS({"RCB": true, "DVT": false, "BSC": true, "SRC": true});
 
+// version
+VERSION = "1.0.0"
+console.log("v"+VERSION);
+
+// vars
 var curr_char = null;
 var buid = null;
 var url_object = null;
 var gen = false;
+var chars = "Aether <br/> Albedo <br/> Aloy <br/> Amber <br/> Itto <br/> Ayaka <br/> Ayato <br/> Barbara <br/> Beidou <br/> Bennett <br/> Chongyun <br/> Collei <br/> Diluc <br/> Diona <br/> Dori <br/> Eula <br/> Fischl <br/> Ganyu <br/> Gorou <br/> Heizou <br/> Hu Tao <br/> Jean <br/> Kaeya <br/> Kazuha <br/> Keqing <br/> Klee <br/> Kokomi <br/> Kuki Shinobu <br/> Lisa <br/> Mona <br/> Ningguang <br/> Noelle <br/> Qiqi <br/> Raiden <br/> Razor <br/> Rosaria <br/> Kujou Sara <br/> Sayu <br/> Shenhe <br/> Sucrose <br/> Tartaglia <br/> Thoma <br/> Tighnari <br/> Lumine <br/> Venti <br/> Xiangling <br/> Xiao <br/> Xingqiu <br/> Xinyan <br/> Yae Miko <br/> Yanfei <br/> Yelan <br/> Yoimiya <br/> Yun Jin <br/> Zhongli <br/><br/><br/><br/>"
+const triggerConfettis = new Event("confetti");
+let confetti = new Confetti('checkInputBtn');
 
-regenBtn = document.getElementById("regenBtn");
-regenBtn.addEventListener("click", function(event) {event.preventDefault(); genImage();})
+confetti.setCount(150);
+confetti.setSize(1);
+confetti.setPower(50);
+confetti.setFade(true);
+confetti.destroyTarget(false);
 
-reportBtn = document.getElementById("reportBtn");
-reportBtn.addEventListener("click", function(event) {event.preventDefault(); showPopup("Report image", "Please join the <a href='https://discord.gg/fzRdtVh', target='_blank'>discord</a> and provide this : <span style='color: white; background: #2a2a2a;'>"+buid+"</span>", 260, 200);})
+function getElem(id) {return document.getElementById(id)};
 
-checkInputBtn = document.getElementById("checkInputBtn");
+// elems and functions
+regenBtn = getElem("regenBtn");
+reportBtn = getElem("reportBtn");
+checkInputBtn = getElem("checkInputBtn");
+uinput = getElem("uinput");
+inputArea = getElem("input-area");
+resultArea = getElem("result-area");
+char = getElem("char")
+showCharsBtn = getElem("showCharsBtn");
+errorText = getElem("error-text");
+loadSpinner = getElem("load-spinner");
+
+document.addEventListener("keydown", function(e) {if (e.ctrlKey && e.keyCode == 81) {e.preventDefault(); if (gen==false) {genImage();}}});
+uinput.onkeydown = function(event) {if (event.keyCode == 13) {check();}};
+regenBtn.addEventListener("click", function(event) {event.preventDefault(); genImage();});
+reportBtn.addEventListener("click", function(event) {event.preventDefault(); showPopup("Report image", "Please join the <a href='https://discord.gg/fzRdtVh', target='_blank'>discord</a> and provide this : <span style='color: white; background: #2a2a2a;'>"+buid+"</span>", 260, 200);});
 checkInputBtn.addEventListener("click", function(event) {event.preventDefault(); check();})
+showCharsBtn.addEventListener("click", function(event) {event.preventDefault(); showPopup("List of characters names", chars, 250, 500);});
 
-document.getElementById("uinput").onkeydown = function(event) {
-    if (event.keyCode == 13) {
-        check();
-    }
+// change buttons state
+function buttonState(tf, b) {
+    regenBtn.disabled = tf;
+    regenBtn.style.filter = "brightness("+b+")";
+
+    uinput.disabled = tf;
+    uinput.style.filter = "brightness("+b+")";
+    
+    checkInputBtn.disabled = tf;
+    checkInputBtn.style.filter = "brightness("+b+")";
+
+    reportBtn.disabled = tf;
+    reportBtn.style.filter = "brightness("+b+")";
 }
 
 function genImage() {
     gen = true;
-    document.getElementById("error-text").style.visibility = "hidden";
-    document.getElementById("load-spinner").style.visibility = "visible";
-    document.getElementById("char").style.opacity = 0.5;
-    
-    // disable stuff
-    regenBtn.disabled = true;
-    regenBtn.style.filter = "brightness(0.7)";
+    errorText.style.visibility = "hidden";
+    loadSpinner.style.visibility = "visible";
+    char.style.opacity = 0.5;
 
-    document.getElementById("uinput").disabled = true;
-    document.getElementById("uinput").style.filter = "brightness(0.7)";
-    
-    checkInputBtn.disabled = true;
-    checkInputBtn.style.filter = "brightness(0.7)";
-
-    reportBtn.disabled = true;
-    reportBtn.style.filter = "brightness(0.7)";
+    buttonState(true, 0.7);
 
     var img = new Image();
 
     img.onload = function() {
-        document.getElementById("char").src = img.src;
-        document.getElementById("char").style.opacity = 1;
-        document.getElementById("load-spinner").style.visibility = "hidden";
+        char.src = img.src;
+        char.style.opacity = 1;
+        loadSpinner.style.visibility = "hidden";
         
-        // enable stuff
-        regenBtn.disabled = false;
-        regenBtn.style.filter = "brightness(1)";
+        buttonState(false, 1);
 
-        document.getElementById("uinput").disabled = false;
-        document.getElementById("uinput").style.filter = "brightness(1)";
-        
-        checkInputBtn.disabled = false;
-        checkInputBtn.style.filter = "brightness(1)";
+        inputArea.style.opacity = 1;
+        resultArea.style.opacity = 0;
 
-        reportBtn.disabled = false;
-        reportBtn.style.filter = "brightness(1)";
-
-        document.getElementById("input-area").style.opacity = 1;
-        document.getElementById("result-area").style.opacity = 0;
-
-        document.getElementById("uinput").focus();
+        uinput.focus();
 
         // for some reason this does not seem to work
         URL.revokeObjectURL(url_object);
@@ -85,20 +97,21 @@ function genImage() {
         });
     })
     .catch((error) => {
-        document.getElementById("char").src = "./assets/img/load.png";
-        document.getElementById("char").style.opacity = 1;
-        document.getElementById("load-spinner").style.visibility = "hidden";
-        document.getElementById("error-text").style.visibility = "visible";
+        char.src = "./assets/img/load.png";
+        char.style.opacity = 1;
+        loadSpinner.style.visibility = "hidden";
+        errorText.style.visibility = "visible";
         regenBtn.disabled = false;
         regenBtn.style.filter = "brightness(1)";
     });
 }
 
 function check() {
-    var entry = document.getElementById("uinput").value;
-    var text = document.getElementById("result-text");
+    var entry = uinput.value;
+    var text = getElem("result-text");
 
     if (entry.toLowerCase() == curr_char.toLowerCase()) {
+        checkInputBtn.dispatchEvent(triggerConfettis)
         text.innerHTML = "Correct 🎉";
         text.style.color = "#beffbe";
     } else {
@@ -106,49 +119,33 @@ function check() {
         text.style.color = "#ff9f9f";
     }
 
-    document.getElementById("uinput").value = "";
+    uinput.value = "";
 
-    document.getElementById("input-area").style.opacity = 0;
-    document.getElementById("result-area").style.opacity = 1;
+    inputArea.style.opacity = 0;
+    resultArea.style.opacity = 1;
 }
 
-document.addEventListener("keydown", function(e) {
-    if (e.ctrlKey && e.keyCode == 81) {
-        e.preventDefault();
-        if (gen == false) {
-            genImage();
-        }
-    }
-})
-
 function showPopup(title, text, width="150", height="90") {
-    var popupDialog = document.getElementById("popupDialog");
+    var popupDialog = getElem("popupDialog");
 
     if (! popupDialog.showModal) {
         dialogPolyfill.registerDialog(popupDialog);
     }
 
-    document.getElementById("popupTitle").innerHTML = title;
-    document.getElementById("popupContent").innerHTML = text;
+    getElem("popupTitle").innerHTML = title;
+    getElem("popupContent").innerHTML = text;
 
-    popupDialog.style.height = height + "px";
-    popupDialog.style.width = width + "px";
+    popupDialog.style.height = height+"px";
+    popupDialog.style.width = width+"px";
 
     popupDialog.showModal();
 
-    popupDialog.querySelector('.close').addEventListener('click', function() {
-        popupDialog.close();
-    });
+    popupDialog.querySelector('.close').addEventListener('click', function() {popupDialog.close();});
 }
-
-chars = "Aether <br/> Albedo <br/> Aloy <br/> Amber <br/> Itto <br/> Ayaka <br/> Ayato <br/> Barbara <br/> Beidou <br/> Bennett <br/> Chongyun <br/> Collei <br/> Diluc <br/> Diona <br/> Dori <br/> Eula <br/> Fischl <br/> Ganyu <br/> Gorou <br/> Heizou <br/> Hu Tao <br/> Jean <br/> Kaeya <br/> Kazuha <br/> Keqing <br/> Klee <br/> Kokomi <br/> Kuki Shinobu <br/> Lisa <br/> Mona <br/> Ningguang <br/> Noelle <br/> Qiqi <br/> Raiden <br/> Razor <br/> Rosaria <br/> Kujou Sara <br/> Sayu <br/> Shenhe <br/> Sucrose <br/> Tartaglia <br/> Thoma <br/> Tighnari <br/> Lumine <br/> Venti <br/> Xiangling <br/> Xiao <br/> Xingqiu <br/> Xinyan <br/> Yae Miko <br/> Yanfei <br/> Yelan <br/> Yoimiya <br/> Yun Jin <br/> Zhongli <br/><br/><br/><br/>"
-showCharsBtn = document.getElementById("showCharsBtn");
-showCharsBtn.addEventListener("click", function(event) {event.preventDefault(); showPopup("List of characters names", chars, 250, 500);})
 
 window.onload = genImage();
 
 // TODO
-// * blob delete ?
-// * confetis
-// * clean assets
-// * phone support
+// * blob files does not seems to be revoked correctly
+// * phone support sucks
+// * support for multiples languages
