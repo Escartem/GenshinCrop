8c0e9e6cf318eba1e4662bcfe8aea1b898f7a267
Escartem <escartem.yt@gmail.com>
2023-10-11 10:50:54 +0200

    2.4.3

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -4,7 +4,7 @@
 setupEMS({"RCB": true, "DVT": false, "BSC": true, "SRC": true});
 
 // version
-VERSION = "2.4.1";
+VERSION = "2.4.3";
 console.log("v"+VERSION);
 document.getElementById("version").innerHTML = "V"+VERSION;
 
